12bafa5b97f104c819870ebbb2be216cd375220a
Cayden Lund <cayden.lund@utah.edu>
2022-08-29 12:33:01 -0600

    Create a complete, working version of `fff`.
    TODO: Multithreading and exception formatting.

--- fff/fff.cpp
+++ fff/fff.cpp
@@ -1,31 +1,31 @@
 /**
  * @file fff.cpp
- * @brief `fff`: Find my freaking file/folder.
+ * @brief `fff`: Find my file or folder.
  * @details Recursively searches for a file or place
  *     in a breadth-first manner.
  *     Supports the following options:
  *
- *     "-name <name>"   --  Search for a file or place with the given name
- *                          or pattern.
- *     "-iname <name>"  --  Same as above, but ignores case.
- *     "-type <t>"      --  Searches for a place of the given type.
- *                          Types:
- *                              - f/file  -  file
- *                              - d/dir   -  directory
- *     "-mindepth <d>"  --  Begin the search at the given depth.
- *     "-maxdepth <d>"  --  End the search at the given depth.
+ *     "-name <name>"     --  Search for a file or folder with the given name
+ *                            or pattern.
+ *     "-iname <name>"    --  Same as above, but ignores case.
+ *
+ *     "-regex <regex>"   --  Search for a file or folder that matches the given
+ *                            regular expression.
+ *     "-iregex <regex>"  --  Same as above, but ignores case.
+ *
+ *     "-j <threads>"     --  The number of threads to use. (Unimplemented.)
+ *
+ *     "-mindepth <d>"    --  Begin the search at the given depth.
+ *     "-maxdepth <d>"    --  End the search at the given depth.
+ *
+ *     "-type <t>"        --  Searches for a place of the given type.
+ *                            Types:
+ *                                - f/file  -  file
+ *                                - d/dir   -  directory
  *
  */
 
-// TODO: arg.h.
 // TODO: Multithreading.
-
-// TODO: Remove these temporary definitions.
-// #define dbg std::cout << "[DEBUG]  "
-// #define red "\033[0;31m"
-// #define green "\033[0;32m"
-// #define blue "\033[0;34m"
-// #define reset_code "\033[0m"
 
 #include <filesystem>
 #include <iostream>
@@ -34,6 +34,8 @@
 #include <string>
 #include <unordered_set>
 
+#include "arg.h"
+
 /**
  * @brief Represents a single place to search, including a depth and a
  * place.
@@ -41,7 +43,7 @@
  */
 struct place {
     /**
-     * @brief The place represented by this struct.
+     * @brief The path represented by this instance.
      *
      */
     std::string path;
@@ -56,6 +58,7 @@ struct place {
 #pragma ide diagnostic ignored "google-explicit-constructor"
     /**
      * @brief Constructor.
+     * @details Can be implicitly constructed from a string path.
      *
      * @param path The path that this `place` represents.
      * @param depth The depth of this `place` in the search space.
@@ -64,6 +67,28 @@ struct place {
         : path(std::move(path)), depth(depth) {}
 #pragma clang diagnostic pop
 };
+
+/**
+ * @brief Translates wildcard syntax into a regular expression pattern.
+ *
+ * @param pattern The pattern to be translated into the regular expression.
+ * @return The translated pattern.
+ */
+std::string wild_to_regex(const std::string &pattern) {
+    std::stringstream result;
+    result << "^";
+
+    for (char c : pattern) {
+        if (c == '*') {
+            result << ".*";
+        } else {
+            result << c;
+        }
+    }
+
+    result << "$";
+    return result.str();
+}
 
 /**
  * @brief Main program entry point.
@@ -75,22 +100,18 @@ struct place {
 int main(int argc, char **argv) {
     /**
      * @brief The type of thing that we're looking for.
+     * @details Defaults to search for everything.
      *
      */
     enum search_types { file, directory, any };
     search_types search_type = search_types::any;
 
     /**
-     * @brief Whether we're ignoring case in our search.
+     * @brief The pattern to match.
+     * @details Defaults to match everything.
      *
      */
-    bool ignore_case;
-
-    /**
-     * @brief The name or pattern to match.
-     *
-     */
-    std::string name;
+    std::regex pattern = std::regex(".*");
 
     /**
      * @brief The places_queue to search.
@@ -117,41 +138,107 @@ int main(int argc, char **argv) {
      */
     unsigned int max_depth = -1;
 
-    // Process arguments.
-    for (unsigned int index = 1; index < argc; index++) {
-        std::string arg(argv[index]);
+    /**
+     * @brief The number of threads to use.
+     *
+     */
+    unsigned int threads = 1;
 
-        if (arg == "-name") {
-            ignore_case = false;
-            name = std::string(argv[++index]);
-        } else if (arg == "-iname") {
-            ignore_case = true;
-            name = std::string(argv[++index]);
-        } else if (arg == "-type") {
-            std::string next(argv[++index]);
-            if (next == "f" || next == "file") {
-                search_type = search_types::file;
-            } else if (next == "d" || next == "dir" || next == "place") {
-                search_type = search_types::directory;
-            } else {
-                std::cerr << "Usage:" << std::endl
-                          << "    " << argv[0] << "-type <t> [...]" << std::endl
-                          << "        Where <t> is one of: {f, file, d, dir, "
-                             "place}."
-                          << std::endl;
-                exit(1);
+    /**
+     * @brief Processes the program arguments.
+     *
+     */
+    argh args(argc, argv);
+
+    // First, select the pattern.
+    // The argument checks are ordered from most specific to least specific.
+    std::string pattern_str[] = {"-regex", "-iregex", "-name", "-iname"};
+    for (unsigned short index = 0; index < 4; index++) {
+        if (args[pattern_str[index]] > 0) {
+            switch (index) {
+                // "-regex":
+                case 0:
+                    pattern = std::regex(args("-regex"));
+                    break;
+                // "-iregex":
+                case 1:
+                    pattern = std::regex(args("-iregex"),
+                                         std::regex_constants::icase);
+                    break;
+                // "-name":
+                case 2:
+                    pattern = std::regex(wild_to_regex(args("-name")));
+                    break;
+                // "-iname":
+                case 3:
+                    pattern = std::regex(wild_to_regex(args("-iname")),
+                                         std::regex_constants::icase);
+                    break;
+                default:
+                    break;
             }
-        } else if (arg == "-mindepth") {
-            std::string next(argv[++index]);
-            min_depth = std::stoi(next);
-        } else if (arg == "-maxdepth") {
-            std::string next(argv[++index]);
-            max_depth = std::stoi(next);
-        } else {
-            // Otherwise, it's one of the "root" files or directories to search.
-            places_queue.emplace_back(arg);
-            searched.insert(arg);
         }
+    }
+
+    // Next, handle the type of search.
+    if (args["-type"] > 0) {
+        if (args("-type") == "d" || args("-type") == "directory") {
+            search_type = search_types::directory;
+        } else if (args("-type") == "f" || args("-type") == "file") {
+            search_type = search_types::file;
+        } else if (args("-type") == "a" || args("-type") == "any") {
+            search_type = search_types::any;
+        } else {
+            std::cerr << "Error: unknown search type \"" << args("-type")
+                      << "\"." << std::endl;
+            exit(1);
+        }
+    }
+
+    // After that, handle the depth arguments.
+    if (args["-mindepth"] > 0) {
+        min_depth = std::stoi(args("-mindepth"));
+    }
+    if (args["-maxdepth"] > 0) {
+        max_depth = std::stoi(args("-maxdepth"));
+    }
+    if (max_depth < min_depth) {
+        std::cerr << "Error: max depth is less than min depth." << std::endl;
+        exit(1);
+    }
+
+    // Handle the threads' argument, if given:
+    if (args["-j"] > 0) {
+        threads = std::stoi(args("-j"));
+
+        if (threads < 1) {
+            std::cerr << "Error: must use at least 1 thread." << std::endl;
+            exit(1);
+        }
+    }
+
+    // Finally, handle the "-h" and "--help" commands.
+    if (args["-h"] + args["--help"] > 0) {
+        std::cout << "Usage:" << std::endl
+                  << "    " << argv[0]
+                  << " <d> <p> [-type <t>] [-mindepth <i>] [-maxdepth <a>]"
+                  << std::endl
+                  << "        Where <d> is one or more files or directories."
+                  << std::endl
+                  << "        Where <p> is one of: {-name, -iname, -regex, "
+                     "-iregex} <pattern>."
+                  << std::endl
+                  << "        Where <t> is one of: {f, file, d, dir, place}."
+                  << std::endl;
+
+        exit(0);
+    }
+
+    // The remaining arguments are files or directories to search.
+    for (size_t index = 1; index < args.size(); index++) {
+        std::string place(args[index]);
+        places_queue.emplace_back(place);
+        searched.emplace(place);
     }
 
     // Begin search.
@@ -192,9 +279,7 @@ int main(int argc, char **argv) {
             std::string short_name =
                 std::filesystem::path(current_place.path).filename();
 
-            // TODO: Patterns.
-            // TODO: Regular expressions.
-            if (short_name == name) {
+            if (std::regex_match(short_name, pattern)) {
                 std::cout << current_place.path << std::endl;
                 continue;
             }
